2ef4f5805d54451cbb20380190c28181d7d895bb
dragonccm <121989450+dragonccm@users.noreply.github.com>
2023-02-05 19:41:57 +0700

    cái đầut

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+import { Text, View } from 'react-native'
+
+const HomeScreen: React.FC = () => {
+  return (
+    <View>
+      <Text>Hello World</Text>
+      <button> nút dầu tiên</button>
+    </View>
+  )
+}
+
+export default HomeScreen
